b352df2462f01155c488c6e25d2edf1b80001af2
heartbeat <heartbeat.webartist@gmail.com>
2021-10-13 23:19:34 +0800

    Add files.

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('login/twitter', 'App\Http\Controllers\Auth\LoginController@redirectToProvider')->name('loginWithFacebook');
+Route::get('login/facebook/callback', 'App\Http\Controllers\Auth\LoginController@handleProviderCallback');
